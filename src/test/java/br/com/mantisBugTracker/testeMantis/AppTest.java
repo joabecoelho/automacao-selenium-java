9fbaf1643e2e5dc0dc59ccb5aaf94608e7ce6856
joabe trabalho <contatojoabe@icloud.com>
2021-07-16 16:35:52 -0300

    adicão do teste Criar Tarefa - novo metodo para verificar existencia de elemento

--- src/test/java/br/com/mantisBugTracker/testeMantis/AppTest.java
+++ src/test/java/br/com/mantisBugTracker/testeMantis/AppTest.java
@@ -2,6 +2,7 @@ package br.com.mantisBugTracker.testeMantis;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import javax.swing.JOptionPane;
@@ -18,12 +19,21 @@ public class AppTest {
 
 	String url;
 	WebDriver driver;
+	
+	private boolean verificarElemento(String id) {
+		try {
+			driver.findElement(By.id(id));
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+		return true;
+	}
 
 	@BeforeEach
 	public void iniciar() {
 		url = "https://www.mantisbt.org/bugs/login_page.php";
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\joabe\\Documents\\Projetos Eclipse\\Testes\\drivers\\chromedriver.exe");
+				"C:\\Users\\joabe\\Documents\\Projetos Eclipse\\automacao-teste\\drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.MILLISECONDS);
@@ -68,6 +78,58 @@ public class AppTest {
 
 		assertEquals(textFalha,
 				driver.findElement(By.xpath("//*[@id=\"main-cont" + "ainer\"]/div/div/div/div/div[4]/p")).getText());
+	}
+	
+	@Test
+	public void testeCriarTarefa() throws InterruptedException {
+		driver.get(url);
+		
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("joabecoelho");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[2]")).click();
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[3]")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/ul/li[1]/div/a")).click();
+		driver.findElement(By.id("category_id")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[1]/td/select/option[2]")).click();
+		driver.findElement(By.id("reproducibility")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[2]/td/select/option[1]")).click();
+		driver.findElement(By.id("severity")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[3]/td/select/option[1]")).click();
+		driver.findElement(By.id("priority")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[4]/td/select/option[4]")).click();
+		boolean t = verificarElemento("profile_open_link");
+		if(t = true) {
+			driver.findElement(By.id("platform")).sendKeys("Teste");
+			driver.findElement(By.id("os")).sendKeys("Windows");
+			driver.findElement(By.id("os_build")).sendKeys("1223_032");
+			driver.findElement(By.id("product_version")).click();
+			driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[6]/td/select/option[3]")).click();
+			driver.findElement(By.id("summary")).sendKeys("testetestesteste");
+			driver.findElement(By.id("description")).sendKeys("testestestestetestestestestetestestestestetestestestestetestestesteste");
+			driver.findElement(By.id("steps_to_reproduce")).sendKeys("testetestesteste");
+			driver.findElement(By.id("additional_info")).sendKeys("testetestesteste");
+			driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[12]/td/label[2]/span")).click();
+			driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[2]/input")).click();
+			Thread.sleep(2000);
+		} else {
+			
+		driver.findElement(By.id("profile_closed_link")).click();	
+			
+		driver.findElement(By.id("platform")).sendKeys("Teste");
+		driver.findElement(By.id("os")).sendKeys("Windows");
+		driver.findElement(By.id("os_build")).sendKeys("1223_032");
+		driver.findElement(By.id("product_version")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[6]/td/select/option[3]")).click();
+		driver.findElement(By.id("summary")).sendKeys("testetestesteste");
+		driver.findElement(By.id("description")).sendKeys("testestestestetestestestestetestestestestetestestestestetestestesteste");
+		driver.findElement(By.id("steps_to_reproduce")).sendKeys("testetestesteste");
+		driver.findElement(By.id("additional_info")).sendKeys("testetestesteste");
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[1]/div/table/tbody/tr[12]/td/label[2]/span")).click();
+		driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[2]/div/div/form/div/div[2]/div[2]/input")).click();
+		Thread.sleep(2000);
+		}
 	}
 
 }
