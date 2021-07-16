446b3a3802b14b27ebe5d63026cc35b44720b08e
joabecoelho <contatojoabe@icloud.com>
2021-07-16 13:24:32 -0300

    alteração de implicitywait para miliseconds - alteração de driver para ChromeDriver

--- src/test/java/br/com/mantisBugTracker/testeMantis/AppTest.java
+++ src/test/java/br/com/mantisBugTracker/testeMantis/AppTest.java
@@ -6,69 +6,68 @@ import java.util.concurrent.TimeUnit;
 
 import javax.swing.JOptionPane;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
-	
+
 	String url;
 	WebDriver driver;
 
 	@BeforeEach
-    public void iniciar() {
-    	url = "https://www.mantisbt.org/bugs/login_page.php";
-    	System.setProperty("webdriver.edge.driver", "C:\\PROJETOS ECLIPSE\\taskit\\drivers\\msedgedriver.exe");
-    	driver = new EdgeDriver();
-    	driver.manage().window().maximize();
-	    driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-    }
-	
-	@AfterEach
-    public void finalizar() {
-    	driver.quit();
-    }
-	
-    @Test
-    public void testeLoginSucesso() throws InterruptedException {
-    	
-    	driver.get(url);
-	   	    	
-	    driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("joabecoelho");
-	    driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[2]")).click();
-	    driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123");
-	    driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[3]")).click();
-	    Thread.sleep(3000);
-	    
-	    finalizar();
-    
-    }
-    @Test
-    public void testeLoginFalho() throws InterruptedException{
-    	
-    	driver.get(url);
-	    
-	    driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("joabecoelho");
-	    driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[2]")).click();
-	    driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("321");
-	    driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[3]")).click();
-	    Thread.sleep(3000);
-	    
-	    String textFalha = "Sua conta pode estar desativada ou bloqueada "
-	    + "ou o nome de usuário e a senha que você digitou não estão corretos.";
-	    
-	    assertEquals(textFalha, driver.findElement(By.xpath("//*[@id=\"main-cont"
-	    + "ainer\"]/div/div/div/div/div[4]/p")).getText());
-	    
-	    finalizar();
-	    
-	    JOptionPane.showMessageDialog(null, "Teste realizado com sucesso.");
-    }
+	public void iniciar() {
+		url = "https://www.mantisbt.org/bugs/login_page.php";
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\joabe\\Documents\\Projetos Eclipse\\automacao-teste\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(6000, TimeUnit.MILLISECONDS);
+	}
 
-    
-    
-    
+	@AfterEach
+	public void finalizar() {
+		driver.quit();
+	}
+
+	@AfterAll
+	public static void aposFinalizarTodos() {
+		JOptionPane.showMessageDialog(null, "Todos os teste(s) finalizado(s).");
+	}
+
+	@Test
+	public void testeLoginSucesso() throws InterruptedException {
+
+		driver.get(url);
+
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("joabecoelho");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[2]")).click();
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[3]")).click();
+		Thread.sleep(2000);
+
+	}
+
+	@Test
+	public void testeLoginFalho() throws InterruptedException {
+
+		driver.get(url);
+
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("joabecoelho");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[2]")).click();
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("321");
+		driver.findElement(By.xpath("//*[@id=\"login-form\"]/fieldset/input[3]")).click();
+		Thread.sleep(2000);
+
+		String textFalha = "Sua conta pode estar desativada ou bloqueada ou o "
+				+ "nome de usuário e a senha que você digitou não estão corretos.";
+
+		assertEquals(textFalha,
+				driver.findElement(By.xpath("//*[@id=\"main-cont" + "ainer\"]/div/div/div/div/div[4]/p")).getText());
+	}
+
 }
